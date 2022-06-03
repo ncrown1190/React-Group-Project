2df16a9cb8a26c6c281ece7003d58f88fc900210
ncrown1190 <ncrown88@outlook.com>
2022-06-02 21:34:18 -0400

    Design folder created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
-export default function Header () {
-    return (
-        <div>
-            <h1>Digital Chef</h1>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div>
+      <img src="" alt="chef icon" />
+      <h1>Digital Chef</h1>
+    </div>
+  );
 }
