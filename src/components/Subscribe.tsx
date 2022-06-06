fee19051f80cdcc25a5f6556ef6149c7e46b3c61
akatko <kateleykohler@gmail.com>
2022-06-07 02:44:59 +0300

    typographical updates

--- src/components/Subscribe.tsx
+++ src/components/Subscribe.tsx
@@ -4,7 +4,7 @@ import "./Subscribe.css";
 export default function Subscribe() {
   return (
     <div className="subscribeContainer">
-      <h3 className="subscribeText">Email us to get the latest recipease</h3>
+      <h3 className="subscribeText">Email us to get the latest recipes</h3>
       <form className="subscribeForm">
         <input type="text" id="emailInput" placeholder="  email" />
         <button className="subscribeBtn">Submit</button>
