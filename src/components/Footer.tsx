e4328566526d7e1aae0eaf219c4f9cb3da7b4324
ncrown1190 <ncrown88@outlook.com>
2022-06-06 20:43:07 -0400

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,17 @@
-export default function Footer () {
-    return(
-        <div>
-            <h4>The icons go here</h4>
-            </div>
-    )
+import "./Footer.css";
+import twitterIcon from "../assets/twitter.svg";
+import facebookIcon from "../assets/facebook.svg";
+import instagramIcon from "../assets/instagram.svg";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="logo-name">Digital Chef</div>
+      <div className="icons-container">
+        <img src={instagramIcon} alt="Instagram Icon" />
+        <img src={twitterIcon} alt="Twitter Icon" />
+        <img src={facebookIcon} alt="Facebook Icon" />
+      </div>
+    </footer>
+  );
 }
